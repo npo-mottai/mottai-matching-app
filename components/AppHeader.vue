28ee154ff2fb04c834fdfcd77dedbfa952aeab4f
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-06-06 22:01:05 +0900

    feat: wip: implement top page

---
+++ components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mx-auto">
+    <div class="flex justify-between h-16 bg-gray-100 px-4">
+      <p class="my-auto">ここにアプリのロゴ</p>
+      <div class="flex my-auto">
+        <outline-search-icon class="ml-4 w-6 h-6" />
+        <outline-bell-icon class="ml-4 w-6 h-6" />
+        <outline-user-circle-icon class="ml-4 w-6 h-6" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class extends Vue {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,224 @@
 <template>
-  <div>Hello World!</div>
+  <div class="">
+    <div class="bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 px-4">
+      <h1>報酬は食べ物！<br />農業 1 日体験アルバイト</h1>
+    </div>
+    <div class="w-screen" style="background-color: #e6f2ff">
+      <div class="container mx-auto">
+        <div class="work-list-section px-4 py-16">
+          <h2>募集中の仕事</h2>
+          <div class="lg:grid lg:grid-cols-2 lg:gap-4">
+            <div class="flex w-full pb-8">
+              <p class="pr-4 my-0">
+                <img
+                  src="/img/yago-san.jpg"
+                  class="h-18 w-24 object-contain rounded-md"
+                  alt="work-thumbnail-image"
+                />
+              </p>
+              <div class="my-0">
+                <h3>みかん農家で果物の収穫体験</h3>
+                <ul>
+                  <li class="flex items-center">
+                    <outline-map-icon class="inline w-4 h-4 mr-1" />
+                    <span>場所：神奈川県小田原市石橋 322</span>
+                  </li>
+                  <li class="flex items-center">
+                    <outline-location-marker-icon class="inline w-4 h-4 mr-1" />
+                    <span>最寄り駅：JR 早川駅, JR 小田原駅</span>
+                  </li>
+                  <li class="flex items-center">
+                    <outline-gift-icon class="inline w-4 h-4 mr-1" />
+                    <span>報酬：旬の果物</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="flex w-full pb-8">
+              <p class="pr-4 my-0">
+                <img
+                  src="/img/yago-san.jpg"
+                  class="h-18 w-24 object-contain rounded-md"
+                  alt="work-thumbnail-image"
+                />
+              </p>
+              <div class="my-0">
+                <h3>みかん農家で果物の収穫体験</h3>
+                <ul>
+                  <li class="flex items-center">
+                    <outline-map-icon class="inline w-4 h-4 mr-1" />
+                    <span>場所：神奈川県小田原市石橋 322</span>
+                  </li>
+                  <li class="flex items-center">
+                    <outline-location-marker-icon class="inline w-4 h-4 mr-1" />
+                    <span>最寄り駅：JR 早川駅, JR 小田原駅</span>
+                  </li>
+                  <li class="flex items-center">
+                    <outline-gift-icon class="inline w-4 h-4 mr-1" />
+                    <span>報酬：旬の果物</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="flex">
+            <a>募集中の仕事をもっと見る </a>
+            <outline-arrow-narrow-right-icon class="ml-4 w-6 h-6" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-screen" style="background-color: #eff3f8">
+      <div class="container mx-auto">
+        <div class="service-description-section px-4 py-16">
+          <h2>サービスについて</h2>
+          <p class="pb-4">
+            “アプリ名” は NPO 法人 MOTTAI
+            が運営する「報酬は食べ物」をテーマとした農業体験アルバイトのマッチングサービスです。
+          </p>
+          <p class="pb-4">
+            「労働」とその対価としての「お金」の交換ではなく、「農業体験・お手伝い」と「報酬として野菜や果物を渡す」という形式の新しいコミュニケーションをつくることで、農家さんと私たちの間に新しい関係性が生まれたり、農家さんが抱えている規格外野菜などの良い行き場になったりすることを期待しています。
+          </p>
+          <p class="pb-4">
+            都市生活では日常振れる機会の少ない貴重な農業体験として、素敵な農家さんたちとの交流の場として、農業のお手伝いをすることで美味しい野菜や果物を貰えるイベントとして、「報酬は食べ物」の農業体験アルバイトに応募してみませんか？
+          </p>
+          <span
+            >※ 現在「アプリ名」は β 版としてサービスを提供しております。</span
+          >
+        </div>
+      </div>
+    </div>
+    <div class="w-screen" style="background-color: #e6f2ff">
+      <div class="container mx-auto">
+        <div class="steps-to-matching-section px-4 py-16">
+          <h2>マッチングまでの流れ</h2>
+          <div class="flex pb-4">
+            <div
+              class="flex-none h-8 w-8 mr-4 rounded-md"
+              style="background-color: #3ea8ff"
+            >
+              <div class="table text-center h-full w-full">
+                <span class="table-cell align-middle font-semibold text-white"
+                  >1</span
+                >
+              </div>
+            </div>
+            <p>
+              「募集中の仕事」から農業体験でお手伝いしてみたい農家さんを探しましょう。
+            </p>
+          </div>
+          <div class="flex pb-4">
+            <div
+              class="flex-none h-8 w-8 mr-4 rounded-md"
+              style="background-color: #3ea8ff"
+            >
+              <div class="table text-center h-full w-full">
+                <span class="table-cell align-middle font-semibold text-white"
+                  >2</span
+                >
+              </div>
+            </div>
+            <p>
+              気になる農家さんが見つかったら、場所、作業内容、日時、報酬の内容などを確認します。
+            </p>
+          </div>
+          <div class="flex pb-4">
+            <div
+              class="flex-none h-8 w-8 mr-4 rounded-md"
+              style="background-color: #3ea8ff"
+            >
+              <div class="table text-center h-full w-full">
+                <span class="table-cell align-middle font-semibold text-white"
+                  >3</span
+                >
+              </div>
+            </div>
+            <p>
+              連絡を取ってみたい農家さんが決まったら、フォームに必要情報を入力して送信します。
+            </p>
+          </div>
+          <div class="flex pb-4">
+            <div
+              class="flex-none h-8 w-8 mr-4 rounded-md"
+              style="background-color: #3ea8ff"
+            >
+              <div class="table text-center h-full w-full">
+                <span class="table-cell align-middle font-semibold text-white"
+                  >4</span
+                >
+              </div>
+            </div>
+            <p>
+              数日以内に、運営または農家さんから連絡が来ます。日程の調整や必要事項の確認をして、農業体験に出かけましょう！
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-screen" style="background-color: #eff3f8">
+      <div class="container mx-auto">
+        <div class="faq-section px-4 py-16" style="background-color: #eff3f8">
+          <h2>よくある質問</h2>
+          <div class="pb-4">
+            <div class="flex pb-2">
+              <span class="font-semibold italic mr-4">Q. </span>
+              <p class="font-semibold italic">
+                農家さんとはどのように連絡を取り合えばよいですか？
+              </p>
+            </div>
+            <div class="flex pb-2">
+              <span class="mr-4">A. </span>
+              <p>
+                農業体験・お手伝いをしてみたいと思う農家さんが見つかったら、必要事項を入力してフォームを送信して下さい。数日以内に運営または農家さんから、フォームに入力した方法（LINE,
+                SNS, メール,
+                電話など）で連絡が届きますので、その時点でマッチング成立です。その後は日時や必要事項などの詳細をお互いに確認して下さい。
+                β
+                版以降のリリースでは、本アプリケーション内でのメッセージ機能なども実装予定です。
+              </p>
+            </div>
+          </div>
+          <div class="pb-4">
+            <div class="flex pb-2">
+              <span class="font-semibold italic mr-4">Q. </span>
+              <p class="font-semibold italic">土日にも参加できますか？</p>
+            </div>
+            <div class="flex pb-2">
+              <span class="mr-4">A. </span>
+              <p>
+                土日に参加できる農家さんもいらっしゃいます。それぞれの農家さんが募集している仕事の詳細情報から日時などを確認して下さい。
+              </p>
+            </div>
+          </div>
+          <div class="pb-4">
+            <div class="flex pb-2">
+              <span class="font-semibold italic mr-4">Q. </span>
+              <p class="font-semibold italic">
+                最寄り駅までの交通費は出ますか？
+              </p>
+            </div>
+            <div class="flex pb-2">
+              <span class="mr-4">A. </span>
+              <p>
+                通常の「労働をしてお給料をもらう」形式のアルバイトとは異なり、基本的には交通費は支払われない場合が多いです。その代わりに、農業体験・お手伝いと農家さんとの交流を楽しみ、野菜や果物などの「食べ物」を報酬として受け取ります。
+              </p>
+            </div>
+          </div>
+          <div class="pb-4">
+            <div class="flex pb-2">
+              <span class="font-semibold italic mr-4">Q. </span>
+              <p class="font-semibold italic">
+                同じ農家さんのお手伝いに繰り返し行っても良いですか？
+              </p>
+            </div>
+            <div class="flex pb-2">
+              <span class="mr-4">A. </span>
+              <p>
+                はい、一度マッチングが成立したり、それから農業体験に行ったりして連絡先を交換した農家さんとは、その後はお互いで連絡を取り、繰り返しお手伝いに行っても構いません。農家さんご本人にもその旨を確認してみましょう。
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
